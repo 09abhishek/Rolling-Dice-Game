98bc0593e70fec47d34060dc84e1a044db62b4f8
Abhishek Gautam <abhi.gautam@imail.com>
2017-11-17 01:05:30 +0530

    Updated the components of JS
    Updated the CSS properties and added the hold and Roll dice button

--- app.js
+++ app.js
@@ -1,45 +1,120 @@
 /*
-GAME RULES:
 
-- The game has 2 players, playing in rounds
+Code Developed by Abhishek Gautam.
+ver. 1.0.0
+
+GAME RULES/LOGIC:
+
+- The game has 2 players, playing one after one.
+
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn.
+
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
 
 // Defining the all required variables.
 
-var scores, roundscore, activePlayer, dice, zero;
+var scores, roundScore, activePlayer, dice, zero;
 
 scores = [0,0];
-roundscore = 0;
-activePlayer = 1;
+roundScore = 0;
+activePlayer = 0;
 zero = 0;
 
+// Initially we dont want to show the dice.
 document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = zero;
-document.getElementById('score-1').textContent = zero;
-document.getElementById('current-0').textContent = zero;
-document.getElementById('current-1').textContent = zero;
+// All the parameters needs to reset to zero.
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
+////////////// Adding event listners - on click to the the roll button. ///////////////
+ 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 // 1. we are getting a random number (1-6).
 dice = Math.floor(Math.random() * 6) + 1;
 
-// Display the proper dice number.
+// 2. Display the proper dice number.
 var diceDOM =  document.querySelector('.dice');
 diceDOM.style.display = 'block';
 diceDOM.src="dice-" + dice + '.png';												 
-													 });
+// 	3. Update the round score (but shud be reset if one comes).												
+if(dice > 1) {
+	//Add score in the roundscore () of current player.
+roundScore += dice; 
+document.querySelector('#current-' + activePlayer).textContent = roundScore;
 
+console.log( 'Player  : ' + activePlayer + '  playing');
+	console.log('Round-Score :' + roundScore + '  Dice Score :'+ dice);
+}
+	else {
+		// Next Player.
+		nextPlayer();
+		
+	}
+ });
 
+///////////// Adding event listners - on click to the the hold button.////////////
+	
+	document.querySelector('.btn-hold').addEventListener('click', function() {
+	
+		// 1. Add the current score to the global score.
+		
+	scores[activePlayer] += roundScore;
+		
+		// 2. Update the UI
+		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+		
+		// 3. Check if player won the game.
+		if(scores[activePlayer] >= 10){
+			document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+			
+		}
+		else {
+			nextPlayer();
+		}
+		
+		
+		// 4. Next Player.
+		nextPlayer();
+	});
 
-
-
-
-
-
+function nextPlayer() {
+	//Moving to next player logic.
+	activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
+		console.log( 'Player  : ' + activePlayer + '  playing');
+		roundScore = 0;
+	
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+	
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+		
+//		document.querySelector('.player-0-panel').classList.remove('active');
+//		document.querySelector('.player-1-panel').classList.add('active');
+		document.querySelector('.dice').style.display = 'none';
+}
+														 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
