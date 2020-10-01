0eee6da434813eb73ea2fd6ad273bb3494e5e699
pavanskipo <pavanskipo@gmail.com>
2020-10-01 20:43:07 +0530

    Fix winner class; Add logic to hide buttons when a winner declared

--- app.js
+++ app.js
@@ -146,6 +146,8 @@ if(dice > 1) {
 	document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
 	document.querySelector('.dice').style.display = 'none';
 	document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+	document.querySelector('.btn-roll').classList.add('hide-btn');
+	document.querySelector('.btn-hold').classList.add('hide-btn');
 	document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 			
 			gamePlaying = false;
@@ -200,11 +202,13 @@ function init() {
 		document.getElementById('current-1').textContent = '0';
 		document.getElementById('name-0').textContent = 'Player 1';
 		document.getElementById('name-1').textContent = 'Player 2';
-		document.querySelector('.player-0-panel').classList.remove('Winner');
-		document.querySelector('.player-1-panel').classList.remove('Winner');
+		document.querySelector('.player-0-panel').classList.remove('winner');
+		document.querySelector('.player-1-panel').classList.remove('winner');
 		document.querySelector('.player-0-panel').classList.remove('active');
 		document.querySelector('.player-1-panel').classList.remove('active');
 		document.querySelector('.player-0-panel').classList.add('active');
+		document.querySelector('.btn-roll').classList.remove('hide-btn');
+		document.querySelector('.btn-hold').classList.remove('hide-btn');
 		$("#crackId").hide();
 	}
 	
